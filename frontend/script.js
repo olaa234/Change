323a9224f486ebe2d662bd660960008c0c1650f9
Muyiwa Olabinjo <89519352+olaa234@users.noreply.github.com>
2022-09-10 21:21:20 +0200

    backend init

---
+++ frontend/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const navMenu = document.querySelector(".navMenu");
+const navLink = document.querySelector(".navLink");
+const links = document.querySelectorAll(".navLink li");
+const mobileLink = document.querySelector(".navLink_Mobile");
+
+navMenu.addEventListener("click", () => {
+  console.log(mobileLink);
+  mobileLink.classList.toggle("open");
+});
+
+links.forEach((link) => {
+  link.classList.toggle("fade");
+});
+
+let company = `[
+  {
+    companyName: "BlackEnergy",
+    Location: "West-London",
+    "Time-zone": "West-African Gmt",
+    Subsidiary: ["ManDef, Perkins, Llyods, Flutter"],
+    isFintech: true,
+  },
+  {
+    name: "Small Startup",
+    numberOfEmployees: 300,
+    ceo: null,
+    ratings: 4.3,
+  }
+]`;
+
+console.log(JSON.parse(company));
